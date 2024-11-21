810dacae628f02977d17edd6377da0c688d4b6e4
zzoo0123 <0123suh@gmail.com>
2024-11-21 14:41:32 +0900

    feat: Swagger

--- src/main/java/com/plantify/cash/CashApplication.java
+++ src/main/java/com/plantify/cash/CashApplication.java
@@ -2,10 +2,11 @@ package com.plantify.cash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@FeignClient
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableFeignClients
 public class CashApplication {
 
 	public static void main(String[] args) {
