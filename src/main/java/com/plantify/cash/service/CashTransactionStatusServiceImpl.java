69dd15c010abdc3388b6a3e6b7706c18eee4e6a5
seunzu <0123suh@gmail.com>
2024-12-18 17:05:50 +0900

    feat: transactionServiceImpl 수정

--- src/main/java/com/plantify/cash/service/CashTransactionStatusServiceImpl.java
+++ src/main/java/com/plantify/cash/service/CashTransactionStatusServiceImpl.java
@@ -1,9 +1,8 @@
 package com.plantify.cash.service;
 
 import com.plantify.cash.domain.dto.TransactionStatusMessage;
+import com.plantify.cash.domain.entity.Cash;
 import com.plantify.cash.domain.entity.Type;
-import com.plantify.cash.global.exception.ApplicationException;
-import com.plantify.cash.global.exception.errorCode.CashErrorCode;
 import com.plantify.cash.repository.CashRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,10 +20,12 @@ public class CashTransactionStatusServiceImpl implements CashTransactionStatusSe
     @Override
     public void processSuccessfulTransaction(TransactionStatusMessage message) {
         try {
-            cashRepository.findByUserId(message.userId())
-                    .orElseThrow(() -> new ApplicationException(CashErrorCode.USER_NOT_FOUND))
-                    .increase((long) (message.amount() * CASH_REWARD_RATE))
-                    .updateType(Type.GRANT);
+            Cash cash = cashRepository.findByUserId(message.userId())
+                    .orElseGet(() -> cashRepository.save(
+                            new Cash().init(message.userId(), Type.GRANT)
+                    ));
+            cash.increase((long) (message.amount() * CASH_REWARD_RATE));
+            cashRepository.save(cash);
         } catch (Exception e) {
             log.error("Error handling transaction status message: {}", e.getMessage());
         }
