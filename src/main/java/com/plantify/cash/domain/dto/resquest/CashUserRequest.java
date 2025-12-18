4b7fecf8cac9a10bb805fff197c43fcd2e5880fe
seunzu <0123suh@gmail.com>
2025-12-18 23:59:13 +0900

    refactor: 사용자 식별을 SecurityContext 기준으로 통합

--- src/main/java/com/plantify/cash/domain/dto/resquest/CashUserRequest.java
+++ src/main/java/com/plantify/cash/domain/dto/resquest/CashUserRequest.java
@@ -1,19 +1,4 @@
 package com.plantify.cash.domain.dto.resquest;
 
-import com.plantify.cash.domain.entity.Cash;
-import com.plantify.cash.domain.entity.Type;
-
-public record CashUserRequest(
-        Long userId,
-        Long amount,
-        String type
-) {
-
-    public Cash toEntity(Long userId) {
-        return Cash.builder()
-                .userId(userId)
-                .cashBalance(amount)
-                .type(type != null ? Type.valueOf(type) : Type.USE)
-                .build();
-    }
+public record CashUserRequest(Long amount) {
 }
